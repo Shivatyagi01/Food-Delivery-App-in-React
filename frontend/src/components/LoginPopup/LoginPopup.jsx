d9552bffde50514c5fa24fa3f0f8a58ad23d5eeb
shivatyagi01 <shivaworx1@gmail.com>
2025-08-25 11:40:37 +0530

    Change in Login popup files

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -18,14 +18,14 @@ const LoginPopup = ({ setShowLogin }) => {
                     <input type="email" placeholder='Your email' required />
                     <input type="password" placeholder='Password' required />
                 </div>
-                <button>{currentState === "Sign up" > "Create account" > "Login"}</button>
+                <button className='popup-button'>{currentState === "Sign up" ? "Create account" : "Login"}</button>
                 <div className="login-popup-condition">
                     <input type="checkbox" required />
                     <p>By continuing, I agree to the terms of use & privacy policy.</p>
                 </div>
                 {currentState === "Login"
                     ? <p>Create a new account? <span onClick={() => setcurrentState("Sign Up")}>Click here</span></p>
-                    : <p>Already have an account? <span onClick={() => setcurrentState("Log In")}>Login here</span></p>}
+                    : <p>Already have an account? <span onClick={() => setcurrentState("Login")}>Login here</span></p>}
             </form>
         </div>
     )
